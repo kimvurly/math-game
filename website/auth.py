bfa67c75828e9732650d4508fc4377b85f122244
kimberly vu <kimberlyy.vuu@gmail.com>
2025-10-30 23:27:50 -0700

    connect flask pages

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,33 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/addition', methods=['GET', 'POST'])
 def addition():
     if request.method =='POST':
-        button = request.form['enter_button']
+        button = request.form['submit_button']
         if button == "enter_addition":
             # check if answer is right.
                 # go to /subtraction
             # else
                 # display /wrong screen (figure out how to do popup)
-            pass
-    return render_template("addition.html")
+            url = url_for('views.addition')
+        elif button == "exit_button":
+            # return to main page
+            url = url_for('views.home')
+    return redirect(url)
+
+@auth.route('/subtraction', methods=['GET', 'POST'])
+def subtraction():
+    if request.method =='POST':
+        button = request.form['submit_button']
+        if button == "enter_subtraction":
+            # check if answer is right.
+                # go to /subtraction
+            # else
+                # display /wrong screen (figure out how to do popup)
+            url = url_for("views.subtraction")
+        elif button == "exit_button":
+            # return to main page
+            url = url_for('views.home')
+    return redirect(url)
