9c8d2a5e9fd012df2df917296f11460661126082
kimberly vu <kimberlyy.vuu@gmail.com>
2025-10-30 22:34:11 -0700

    trying to style

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,14 @@ from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-# @auth.route('/swiping', methods=['GET', 'POST'])
-# def swiping():
-#     return
+@auth.route('/addition', methods=['GET', 'POST'])
+def addition():
+    if request.method =='POST':
+        button = request.form['enter_button']
+        if button == "enter_addition":
+            # check if answer is right.
+                # go to /subtraction
+            # else
+                # display /wrong screen (figure out how to do popup)
+            pass
+    return render_template("addition.html")
