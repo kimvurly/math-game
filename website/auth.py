9b71920dfd48d5d17c1440fca6acbab8c7d19a31
kimberly vu <kimberlyy.vuu@gmail.com>
2025-10-31 01:22:05 -0700

    make random problem generation, system for counting, auth to move on to next level (subtraction and addition levels)

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,59 @@
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request, url_for, redirect, session
+import random
 
 auth = Blueprint('auth', __name__)
 
+'''
+ note for future self: make the code a separate function and js import it
+ u can change the very small details with parameters, like the template to render
+ and the random problem generation too
+'''
 @auth.route('/addition', methods=['GET', 'POST'])
 def addition():
+    # num of questions right to be able to pass onto next level
+    required_right = 10
+    if 'tries_counter' not in session:
+        session['tries_counter'] = 0
+
+    if 'right_counter' not in session:
+        session['right_counter'] = 0
+
     if request.method =='POST':
-        button = request.form['submit_button']
+        button = request.form.get('submit_button')
+        answer = int(request.form.get('answer'))
+        correct_answer = int(request.form.get('correct_answer'))
+        problem = request.form.get('problem')
+
         if button == "enter_addition":
-            # check if answer is right.
-                # go to /subtraction
-            # else
-                # display /wrong screen (figure out how to do popup)
-            url = url_for('views.addition')
+            if answer == correct_answer:
+                session['right_counter'] += 1
+                # make them answer a certain number of questions to move on
+                if session['right_counter'] >= required_right:
+                    # reset all session things
+                    session['tries_counter'] = 0
+                    session['right_counter'] = 0
+                    # go to /subtraction if so
+                    url = url_for('views.subtraction')
+                else:
+                    # reset tries counter (just in case im paranoid) and generate new problem
+                    session['tries_counter'] = 0
+                    url = url_for('views.addition')
+            else:
+                if session['tries_counter'] < 3:
+                    error = f"wrong! you have {3 - session['tries_counter']} tries left."
+                    session['tries_counter'] += 1
+                    return render_template("addition.html", problem=problem, answer=correct_answer, error=error)
+                elif session['tries_counter'] == 3:
+                    # display error
+                    error = f"sorry, the answer is {correct_answer}."
+                    # reset counter for next problem
+                    session['tries_counter'] = 0
+                    # generate new problem and answer
+                    a = random.randint(1, 20)
+                    b = random.randint(1, 20)
+                    problem = f"{a} + {b}"
+                    new_answer = a + b
+                    return render_template("addition.html", problem=problem, answer=new_answer, error=error)
         elif button == "exit_button":
             # return to main page
             url = url_for('views.home')
@@ -19,15 +61,60 @@ def addition():
 
 @auth.route('/subtraction', methods=['GET', 'POST'])
 def subtraction():
+    required_right = 10
+    if 'tries_counter' not in session:
+        session['tries_counter'] = 0
+    
+    if 'right_counter' not in session:
+        session['right_counter'] = 0
+
     if request.method =='POST':
-        button = request.form['submit_button']
+        button = request.form.get('submit_button')
+        answer = int(request.form.get('answer'))
+        correct_answer = int(request.form.get('correct_answer'))
+        problem = request.form.get('problem')
+
         if button == "enter_subtraction":
             # check if answer is right.
-                # go to /subtraction
-            # else
-                # display /wrong screen (figure out how to do popup)
-            url = url_for("views.subtraction")
+            if answer == correct_answer:
+                session['right_counter'] += 1
+                # make them answer a certain number of questions to move on
+                if session['right_counter'] >= required_right:
+                    # reset all session things
+                    session['tries_counter'] = 0
+                    session['right_counter'] = 0
+                    # go to /multiplication
+                    url = url_for('views.multiplication')
+                else:
+                    # reset tries counter
+                    session['tries_counter'] = 0
+                    url = url_for('views.subtraction')
+            else:
+                # let the user try the problem 4 times
+                if session['tries_counter'] < 3:
+                    # display number of tries left each time
+                    error = f"wrong! you have {3 - session['tries_counter']} tries left."
+                    session['tries_counter'] += 1
+                    return render_template("subtraction.html", problem=problem, answer=correct_answer, error=error)
+                # if at 4 tries, just display the right answer
+                elif session['tries_counter'] == 3:
+                    error = f"sorry, the answer is {correct_answer}."
+                    # reset counter for next problem (again, paranoid)
+                    session['tries_counter'] = 0
+                    # generate new problem and answer
+                    a = random.randint(1, 100)
+                    b = random.randint(1, 100)
+                    problem = f"{a} - {b}"
+                    new_answer = a - b
+                    return render_template("subtraction.html", problem=problem, answer=new_answer, error=error)
         elif button == "exit_button":
             # return to main page
             url = url_for('views.home')
     return redirect(url)
+
+@auth.route('/multiplication', methods=['GET', 'POST'])
+def multiplication():
+    required_right = 5
+    if 'tries_counter' not in session:
+        session['tries_counter'] = 0
+
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 # standard routes for the website, where users can actually go, not authentication
-from flask import Blueprint, render_template # blueprint of our website, has a bunch of routes
+from flask import Blueprint, render_template, request, render_template, redirect, url_for # blueprint of our website, has a bunch of routes
+import random
 
 views = Blueprint('views', __name__) # sets up blueprint
 
@@ -10,8 +11,27 @@ def home():
 
 @views.route('/addition')
 def addition():
-    return render_template("addition.html")
+    a = random.randint(1, 100)
+    b = random.randint(1, 100)
+    problem = f"{a} + {b}"
+    answer = a + b
+    
+    return render_template("addition.html", problem=problem, answer=answer)
 
 @views.route('/subtraction')
 def subtraction():
-    return render_template("subtraction.html")
+    a = random.randint(1, 100)
+    b = random.randint(1, 100)
+    problem = f"{a} - {b}"
+    answer = a - b
+
+    return render_template("subtraction.html", problem=problem, answer=answer)
+
+@views.route('/multiplication')
+def multiplication():
+    a = random.randint(1, 100)
+    b = random.randint(1, 100)
+    problem = f"{a} x {b}"
+    answer = a*b
+
+    return render_template("multiplication.html")
