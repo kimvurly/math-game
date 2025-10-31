03cf585ecf5181256c20382f2564707be7c8205d
kimberly vu <kimberlyy.vuu@gmail.com>
2025-10-31 08:20:16 -0700

    made more levels (multiplication, division, algebra)

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, url_for, redirect, session
 import random
+import sympy as sp
 
 auth = Blueprint('auth', __name__)
 
@@ -114,7 +115,164 @@ def subtraction():
 
 @auth.route('/multiplication', methods=['GET', 'POST'])
 def multiplication():
-    required_right = 5
+    required_right = 10
     if 'tries_counter' not in session:
         session['tries_counter'] = 0
 
+    if 'right_counter' not in session:
+        session['right_counter'] = 0
+    
+    if request.method == 'POST':
+        button = request.form.get('submit_button')
+        answer = int(request.form.get('answer'))
+        correct_answer = int(request.form.get('correct_answer'))
+        problem = request.form.get('problem')
+
+        if button == "enter_multiplication":
+            # check if answer is right
+            if answer == correct_answer:
+                session['right_counter'] += 1
+                # make them answer a certain number of questions to pass
+                if session['right_counter'] >= required_right:
+                    # reset all session things
+                    session['tries_counter'] = 0
+                    session['right_counter'] = 0
+
+                    # go to /division
+                    url = url_for('views.division')
+                else:
+                    # reset tries counter
+                    session['tries_counter'] = 0
+                    url = url_for('views.subtraction')
+            else:
+                # let the user try the problem 4 times
+                if session['tries_counter'] < 3:
+                    # display number of tries left each time
+                    error = f"wrong! you have {3 - session['tries_counter']} tries left."
+                    session['tries_counter'] += 1
+                # if at 4 tries, display right answer
+                elif session['tries_counter'] == 3:
+                    error = f"sorry, the answer is {correct_answer}."
+                    # reset counter for next problem
+                    session['tries_counter'] = 0
+                    # generate new problem and answer
+                    a = random.randint(1, 100)
+                    b = random.randint(1, 100)
+                    problem = f"{a} x {b}"
+                    new_answer = a*b
+                    return render_template("multiplication.html", problem=problem, answer=new_answer, error=error)
+        elif button == "exit_button":
+            # return to main page
+            url = url_for('views.home')
+    return redirect(url)
+
+@auth.route('/division', methods=['GET', 'POST'])
+def division():
+    required_right = 10
+    if 'tries_counter' not in session:
+        session['tries_counter'] = 0
+
+    if 'right_counter' not in session:
+        session['right_counter'] = 0
+    
+    if request.method == 'POST':
+        button = request.form.get('submit_button')
+        answer = int(request.form.get('answer'))
+        correct_answer = int(request.form.get('correct_answer'))
+        problem = request.form.get('problem')
+
+        if button == "enter_division":
+            # check if answer is right
+            if answer == correct_answer:
+                session['right_counter'] += 1
+                # make them answer a certain number of questions to pass
+                if session['right_counter'] >= required_right:
+                    # reset all session things
+                    session['tries_counter'] = 0
+                    session['right_counter'] = 0
+
+                    # go to end or maybe try doing algebra problems?
+                    url = url_for('views.algebra')
+                else:
+                    # reset tries counter
+                    session['tries_counter'] = 0
+                    url = url_for('views.subtraction')
+            else:
+                # let the user try the problem 4 times
+                if session['tries_counter'] < 3:
+                    # display number of tries left each time
+                    error = f"wrong! you have {3 - session['tries_counter']} tries left."
+                    session['tries_counter'] += 1
+                # if at 4 tries, display right answer
+                    if session['tries_counter'] == 3:
+                        error =f"sorry, the answer is {correct_answer}."
+                        # reset counter for next problem
+                        session['tries_counter'] = 0
+                        # generate new problem and number
+                        a = random.randint(1, 100)
+                        b = a*random.randint(1, 100)
+                        problem = f"{b} / {a}"
+                        new_answer = b/a
+
+                        return render_template("division.html", problem=problem, answer=new_answer, error=error)
+        elif button == "exit_button":
+            # return to main page
+            url = url_for('views.home')
+    return redirect(url)
+
+@auth.route('/algebra', methods=['GET', 'POST'])
+def algebra():
+    required_right = 2
+    if 'tries_counter' not in session:
+        session['tries_counter'] = 0
+
+    if 'right_counter' not in session:
+        session['right_counter'] = 0
+
+    if request.method == 'POST':
+        button = request.form.get('submit_button')
+        answer = request.form.get('answer')
+        correct_answer = request.form.get('correct_answer')
+        problem = request.form.get('problem')
+
+        if button =="enter_algebra_1":
+            # check if answer is right
+            if answer == correct_answer:
+                session['right_counter'] += 1
+                # make them answer a certain number of questions
+                if session['right_counter'] == required_right:
+                    # reset all session things
+                    session['tries_counter'] = 0
+                    session['right_counter'] = 0
+
+                    # go to next algebra level (quadratics)
+                    url = url_for('views.algebra')
+                else:
+                    # reset tries counter
+                    session['tries_counter'] = 0
+                    url = url_for('views.algebra') # change later
+
+            else:
+                # let the user try the problem 4 times
+                if session['tries_counter'] < 3:
+                    error = f"wrong! you have {3 - session['tries_counter']} tries left."
+                    session['tries_counter'] += 1
+                # if at 4 tries, display right answer
+                    if session['tries_counter'] == 3:
+                        error = f"sorry, the answer is {correct_answer}."
+                        # reset counter for next problem
+                        session['tries_counter'] = 0
+                        # generate new problem and number
+                        a = random.randint(1, 20)
+                        b = random.randint(1, 20)
+                        c = random.randint(1, 20)
+                        x= symbols('x')
+                        equation = Eq(a*x + b, c)
+                        problem = f"{a}x + {b} = {c}"
+                        new_answer = solve(equation, x)[0]
+
+                        return render_template("algebra.html", problem=problem, answer=new_answer, error=error)
+        elif button == "exit_button":
+            # return to main page
+            url = url_for('views.home')
+    return redirect(url)
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 # standard routes for the website, where users can actually go, not authentication
 from flask import Blueprint, render_template, request, render_template, redirect, url_for # blueprint of our website, has a bunch of routes
 import random
+from sympy import symbols, Eq, solve
 
 views = Blueprint('views', __name__) # sets up blueprint
 
@@ -11,8 +12,8 @@ def home():
 
 @views.route('/addition')
 def addition():
-    a = random.randint(1, 100)
-    b = random.randint(1, 100)
+    a = random.randint(10, 100)
+    b = random.randint(10, 100)
     problem = f"{a} + {b}"
     answer = a + b
     
@@ -20,8 +21,8 @@ def addition():
 
 @views.route('/subtraction')
 def subtraction():
-    a = random.randint(1, 100)
-    b = random.randint(1, 100)
+    a = random.randint(10, 100)
+    b = random.randint(10, 100)
     problem = f"{a} - {b}"
     answer = a - b
 
@@ -29,9 +30,30 @@ def subtraction():
 
 @views.route('/multiplication')
 def multiplication():
-    a = random.randint(1, 100)
-    b = random.randint(1, 100)
+    a = random.randint(2, 100)
+    b = random.randint(2, 100)
     problem = f"{a} x {b}"
     answer = a*b
 
-    return render_template("multiplication.html")
+    return render_template("multiplication.html", problem=problem, answer=answer)
+
+@views.route('/division')
+def division():
+    a = random.randint(2, 100)
+    b = a * random.randint(1, 100)
+    problem = f"{b} / {a}"
+    answer = b/a
+
+    return render_template("division.html", problem=problem, answer=answer)
+
+@views.route('/algebra')
+def algebra():
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    c = random.randint(1, 20)
+    x= symbols('x')
+    equation = Eq(a*x + b, c)
+    problem = f"{a}x + {b} = {c}"
+    answer = solve(equation, x)[0]
+    
+    return render_template("algebra.html", problem=problem, answer=answer)
